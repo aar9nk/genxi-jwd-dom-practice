73fe04489a772f1ab451b1618d6d655ddbae2f60
nicktho <matenaar92@gmail.com>
2020-09-09 11:16:51 +1000

    add exercises

---
+++ index.js
@@ -0,0 +1,17 @@
+// Buttons
+const displayNameButton = document.querySelector('#display-name-button');
+
+displayNameButton.onclick = () => {
+    const displayName = document.querySelector('#display-name');
+    displayName.classList.remove('invisible');
+};
+
+// Mouse Over
+
+// Toggle
+
+// Validate
+
+// Challenge: Lists
+
+// Challenge: Lists (Part 2)
